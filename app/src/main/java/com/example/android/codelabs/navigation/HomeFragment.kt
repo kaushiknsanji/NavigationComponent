b006670e905e69df45e2356e986dae8f9af5b4c3
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2020-06-18 13:27:21 +0530

    refactor: (TODO: 6) Set NavOptions to include transitions while navigating to 'flow_step_one_dest' fragment

--- app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
+++ app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
@@ -20,7 +20,8 @@ import android.os.Bundle
 import android.view.*
 import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 
 /**
  * Fragment used to show how to navigate to another destination
@@ -40,24 +41,20 @@ class HomeFragment : Fragment() {
 
         // Completed STEP 5 - Set an OnClickListener, using Navigation.createNavigateOnClickListener()
         val button = view.findViewById<Button>(R.id.navigate_destination_button)
+        // Completed STEP 6 - Set NavOptions
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                // Transitions for destinations popped from back stack
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
         // Navigate to flow_step_one_dest fragment on click of the destination button
-        button?.setOnClickListener(
-                Navigation.createNavigateOnClickListener(R.id.flow_step_one_dest, null)
-        )
-
-        //TODO STEP 6 - Set NavOptions
-//        val options = navOptions {
-//            anim {
-//                enter = R.anim.slide_in_right
-//                exit = R.anim.slide_out_left
-//                popEnter = R.anim.slide_in_left
-//                popExit = R.anim.slide_out_right
-//            }
-//        }
-//        view.findViewById<Button>(R.id.navigate_destination_button)?.setOnClickListener {
-//            findNavController().navigate(R.id.flow_step_one_dest, null, options)
-//        }
-        //TODO END STEP 6
+        button?.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null, options)
+        }
 
         //TODO STEP 7.2 - Update the OnClickListener to navigate using an action
 //        view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener(
