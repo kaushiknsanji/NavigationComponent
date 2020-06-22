a873cf67563996836af3fc5a9b8d1fdd8bcc5e8c
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2020-06-22 17:35:10 +0530

    refactor: (TODO: 10) Add deep link to deeplink_dest from App widget

--- app/src/main/java/com/example/android/codelabs/navigation/DeepLinkAppWidgetProvider.kt
+++ app/src/main/java/com/example/android/codelabs/navigation/DeepLinkAppWidgetProvider.kt
@@ -21,32 +21,50 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.os.Bundle
 import android.widget.RemoteViews
+import androidx.navigation.NavDeepLinkBuilder
 
 /**
  * App Widget that deep links you to the [DeepLinkFragment].
  */
 class DeepLinkAppWidgetProvider : AppWidgetProvider() {
+    /**
+     * Called in response to the [AppWidgetManager.ACTION_APPWIDGET_UPDATE] and
+     * [AppWidgetManager.ACTION_APPWIDGET_RESTORED] broadcasts when this AppWidget Provider
+     * is being asked to provide [RemoteViews] for a set of AppWidgets.
+     *
+     * @param context [Context] in which this receiver is running.
+     * @param appWidgetManager [AppWidgetManager] instance to call [AppWidgetManager.updateAppWidget] on.
+     * @param appWidgetIds The appWidgetIds for which an update is needed.  Note that this
+     * may be all of the AppWidget instances for this provider, or just a subset of them.
+     */
     override fun onUpdate(
             context: Context,
             appWidgetManager: AppWidgetManager,
             appWidgetIds: IntArray
     ) {
+        // Construct the RemoteViews
         val remoteViews = RemoteViews(
                 context.packageName,
                 R.layout.deep_link_appwidget
         )
 
+        // Construct the Arguments for the Deep Linked destination
         val args = Bundle()
         args.putString("myarg", "From Widget")
-        // TODO STEP 10 - construct and set a PendingIntent using DeepLinkBuilder
-//        val pendingIntent = NavDeepLinkBuilder(context)
-//                .setGraph(R.navigation.mobile_navigation)
-//                .setDestination(R.id.deeplink_dest)
-//                .setArguments(args)
-//                .createPendingIntent()
-//
-//        remoteViews.setOnClickPendingIntent(R.id.deep_link_button, pendingIntent)
-        // TODO END STEP 10
+
+        // Completed STEP 10 - construct and set a PendingIntent using DeepLinkBuilder
+        // Construct the Pending Intent to launch the Deep Linked destination
+        val pendingIntent = NavDeepLinkBuilder(context)
+                .setGraph(R.navigation.mobile_navigation)
+                .setDestination(R.id.deeplink_dest)
+                .setArguments(args)
+                .createPendingIntent()
+
+        // Register a click handler on the App Widget's Button
+        // to launch the Deep Linked destination with the given Pending Intent
+        remoteViews.setOnClickPendingIntent(R.id.deep_link_button, pendingIntent)
+
+        // Instruct the Widget manager to update the App Widgets
         appWidgetManager.updateAppWidget(appWidgetIds, remoteViews)
     }
 }
