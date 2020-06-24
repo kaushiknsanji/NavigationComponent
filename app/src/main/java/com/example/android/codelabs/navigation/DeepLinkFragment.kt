f5ae424e6754f2018b481d8ba72d2c2dca73fb89
Kaushik N Sanji <26028981+kaushiknsanji@users.noreply.github.com>
2020-06-24 19:52:31 +0530

    refactor: Use Safe Args to access arguments in DeepLinkFragment

--- app/src/main/java/com/example/android/codelabs/navigation/DeepLinkFragment.kt
+++ app/src/main/java/com/example/android/codelabs/navigation/DeepLinkFragment.kt
@@ -30,6 +30,7 @@ import android.widget.TextView
 import androidx.core.app.NotificationCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 
 /**
  * Fragment used to show how to deep link to a destination
@@ -46,35 +47,67 @@ class DeepLinkFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val textView = view.findViewById<TextView>(R.id.text)
-        textView.text = arguments?.getString("myarg")
+        // Set the argument text by reading its value from Bundle arguments via Safe Args
+        val navArgs by navArgs<DeepLinkFragmentArgs>()
+        view.findViewById<TextView>(R.id.text).text = navArgs.myarg
 
+        // Set Click listener on "Send Deep Link Notification" Button to create
+        // and show a Notification for this Deep linked destination
         val notificationButton = view.findViewById<Button>(R.id.send_notification_button)
         notificationButton.setOnClickListener {
+            // Read the value from edit text field and save it as Bundle arguments for the Intent
             val editArgs = view.findViewById<EditText>(R.id.args_edit_text)
             val args = Bundle()
-            args.putString("myarg", editArgs.getText().toString())
+            args.putString("myarg", editArgs.text.toString())
 
-            val deeplink = findNavController().createDeepLink()
+            // Construct a Pending Intent to launch this Deep linked destination from a Notification
+            val deepLink = findNavController().createDeepLink()
                     .setDestination(R.id.deeplink_dest)
-                    .setArguments(args)
+                    .setArguments(args) // Pass the argument bundle built
                     .createPendingIntent()
 
+            // Retrieve the instance of NotificationManager to notify the user of the events
             val notificationManager =
                     context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                // Build a Notification Channel for devices with API level 26+
+                // and register it with the NotificationManager
                 notificationManager.createNotificationChannel(NotificationChannel(
-                        "deeplink", "Deep Links", NotificationManager.IMPORTANCE_HIGH))
+                        DEEP_LINK_CHANNEL_ID,
+                        requireContext().getString(R.string.deep_link_notification_channel_name),
+                        NotificationManager.IMPORTANCE_HIGH
+                ))
             }
 
-            val builder = NotificationCompat.Builder(
-                    context!!, "deeplink")
-                    .setContentTitle("Navigation")
-                    .setContentText("Deep link to Android")
+            // Construct the Notification Content belonging to "Deep Links" Notification channel
+            val builder = NotificationCompat.Builder(requireContext(), DEEP_LINK_CHANNEL_ID)
+                    // Title of the Notification
+                    .setContentTitle(requireContext().getString(R.string.title_deep_link_notification_content))
+                    // Content text of the Notification
+                    .setContentText(requireContext().getString(R.string.text_deep_link_notification_content))
+                    // The required small icon
                     .setSmallIcon(R.drawable.ic_android)
-                    .setContentIntent(deeplink)
+                    // Pending Intent for the Notification which is Deep linked to this destination
+                    .setContentIntent(deepLink)
+                    // Automatically cancel/clear the Notification when clicked
                     .setAutoCancel(true)
-            notificationManager.notify(0, builder.build())
+
+            // Post the Notification with the Notification content built
+            notificationManager.notify(
+                    // Unique identifier for this notification within the app
+                    DEEP_LINK_NOTIFICATION_ID,
+                    // Build and show this notification
+                    builder.build()
+            )
         }
+    }
+
+    companion object {
+        // Constant for the Notification Channel ID
+        const val DEEP_LINK_CHANNEL_ID = "deepLink"
+
+        // Constant for the Notification built for "Deep Links" Notification channel
+        const val DEEP_LINK_NOTIFICATION_ID = 0
     }
 }
